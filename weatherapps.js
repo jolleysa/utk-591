164fe3fecdc31f27de837157171a536ee5b2a157
Stephanie <112589422+jolleysa@users.noreply.github.com>
2022-10-06 20:57:41 -0400

    Update weatherapps.js

--- weatherapps.js
+++ weatherapps.js
@@ -14,36 +14,38 @@ window.addEventListener("load",() => {
 
             let api = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/knoxville%20tn?unitGroup=metric&include=current&key=5HMLCCDVCV2AA889GUPZ3NTH5&contentType=json`;
 
-        fetch(api)
-            .then(response =>{
-                return response.json();
-            })
-            .then(data =>{
-                console.log(data);
-                const {temp, conditions, timezone, icon}= data.currently;
-                
-                //set DOM Elements from the API
-                tempdegree.textContent = temp;
-                tempdescription.textContent = conditions;
-                locationTimezone.textContent = timezone;
-                
-                //FORMULA for celsius
-                let celsius = (temp - 32) * (5/9);
-                
-                //set Icon
-                setIcons(icon, document.querySelector("icon"));
-                
-                //Change temp degree
-                degreesection.addEventListener('click', ()=>{
-                    if(temperatureSpan.textContent === "F") {
-                        temperatureSpan.textContent = "C";
-                        tempdegree.textContent = Math.floor(celsius);
-                    }else{
-                        temperatureSpan.textContent = "F";
-                        tempdegree.textContent = temperature;
-                    }
+            fetch(api)
+                .then(response =>{
+                    return response.json();
                 })
-            });
+                .then(data =>{
+                    console.log(data);
+                    const {temp, conditions, icon}= data.currentConditions;
+
+                    //set DOM Elements from the API
+                    tempdegree.textContent = temp;
+                    tempdescription.textContent = conditions;
+
+                    const {timezone}=data;
+                    locationTimezone.textContent = timezone;
+
+                    //FORMULA for fahrenheit
+                    let fahrenheit = (temp * (9/5) + 32);
+
+                    //set Icon
+                    setIcons(icon, document.querySelector("icon"));
+
+                    //Change temp degree
+                    degreesection.addEventListener('click', ()=>{
+                        if(temperatureSpan.textContent === "C") {
+                            temperatureSpan.textContent = "F";
+                            tempdegree.textContent = Math.floor(fahrenheit);
+                        }else{
+                            temperatureSpan.textContent = "C";
+                            tempdegree.textContent = temp;
+                        }
+                    })
+                });
         });
     }
     function setIcons(icon, iconID) {
